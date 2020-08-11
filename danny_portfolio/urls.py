568c587b25743e2d2fec239df85fbcb19caf068e
Danny Nguyen <dannynguyen626@yahoo.com>
2020-08-10 18:30:37 -0700

    bootstrap overhaul
    applying some bootstrap that i learned over the last couple weeks

--- danny_portfolio/urls.py
+++ danny_portfolio/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path     
 from . import views
 urlpatterns = [
-    path('', views.landing),
+   path('', views.resume),
     path('contact_me', views.contact),
-    path('resume', views.resume),
+    
     path('projects', views.projects),
     path('about_me', views.about)		   
 ]
