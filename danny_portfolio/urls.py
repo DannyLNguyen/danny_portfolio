0145316ebc4bcdb9afe3cc16a3f50c3bb802c867
Danny Nguyen <dannynguyen626@yahoo.com>
2020-09-01 12:07:35 -0700

    clean up before launch
    getting rid of unused pages and templates. combining about me and resume pages

--- danny_portfolio/urls.py
+++ danny_portfolio/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
    path('', views.resume),
-    path('contact_me', views.contact),
-    
-    path('projects', views.projects),
-    path('about_me', views.about)		   
+   	   
 ]
--- danny_portfolio/views.py
+++ danny_portfolio/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
 
-def landing(request):
-    return render(request, "welcome.html")
-
-def contact(request):
-    return render(request, 'contact.html')
 
 def resume(request):
     return render(request, 'resume.html')
 
-def projects(request):
-    return render(request, 'projects.html')
 
-def about(request):
-
-    return render(request, 'about.html')
